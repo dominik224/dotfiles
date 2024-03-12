c2f00d0503f2598593cb7f3aff06a36583989826
Dominik Przychodni <dominik224@interia.pl>
2024-03-12 15:35:07 -0400

    Added packer bootstrap command

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,3 +6,11 @@ cp ./tmux/.tmux.conf ~/.tmux.conf
 echo "Copying nvim to ~/.config/."
 mkdir ~/.config
 cp -r nvim ~/.config/nvim
+
+echo "Bootstrapping Packer"
+echo 'require("dominik.packer")' > ~/.config/nvim/lua/dominik/init.lua
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+
+echo "Restoring nvim configuration"
+cp -r nvim ~/.config/nvim
+
